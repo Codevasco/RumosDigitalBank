b569f9b3f4a734624e6478268dd9d5017d7e3310
Vasco Gonçalves <vascofg99alt@gmail.com>
2023-02-14 15:56:34 +0000

    Repository migration

---
+++ RDB/src/main/java/org/bnpparibas/rdb/RdbApplication.java
@@ -0,0 +1,13 @@
+package org.bnpparibas.rdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdbApplication.class, args);
+    }
+
+}
