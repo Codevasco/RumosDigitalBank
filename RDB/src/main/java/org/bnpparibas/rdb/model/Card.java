fea59a8e8c953763e77fa042155afb4cf7af91f1
Vasco Gonçalves <vascofg99alt@gmail.com>
2023-02-22 11:05:11 +0000

    Card creation

--- RDB/src/main/java/org/bnpparibas/rdb/model/Card.java
+++ RDB/src/main/java/org/bnpparibas/rdb/model/Card.java
@@ -1,4 +1,26 @@
 package org.bnpparibas.rdb.model;
 
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bnpparibas.rdb.model.entity.AccountEntity;
+import org.bnpparibas.rdb.model.entity.ClientEntity;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Card {
+
+    private UUID cardID;
+
+    private Integer cardPin;
+
+    private ClientEntity cardOwner;
+
+    private AccountEntity account;
 }
--- RDB/src/main/java/org/bnpparibas/rdb/model/entity/cards/CardEntity.java
+++ RDB/src/main/java/org/bnpparibas/rdb/model/entity/cards/CardEntity.java
@@ -1,13 +1,33 @@
 package org.bnpparibas.rdb.model.entity.cards;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bnpparibas.rdb.model.entity.AccountEntity;
+import org.bnpparibas.rdb.model.entity.ClientEntity;
 
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "CARD")
 public class CardEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CARD_ID")
     private UUID cardId;
+
+    private Integer cardPin;
+
+    @OneToOne
+    private ClientEntity cardOwner;
+
+    @OneToOne
+    private AccountEntity account;
 }
