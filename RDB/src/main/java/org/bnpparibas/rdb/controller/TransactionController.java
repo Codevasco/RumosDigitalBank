ad0b60680dfd415c5606eb2c40b86ef7e52acc1b
Vasco Gonçalves <vascofg99alt@gmail.com>
2023-03-07 15:22:09 +0000

    Controller changes

--- RDB/src/main/java/org/bnpparibas/rdb/controller/TransactionController.java
+++ RDB/src/main/java/org/bnpparibas/rdb/controller/TransactionController.java
@@ -1,39 +1,73 @@
 package org.bnpparibas.rdb.controller;
 
-import org.bnpparibas.rdb.model.Transaction;
+import jakarta.servlet.http.HttpSession;
+import org.bnpparibas.rdb.model.Account;
+import org.bnpparibas.rdb.model.Client;
 import org.bnpparibas.rdb.model.operations.Deposit;
 import org.bnpparibas.rdb.model.operations.Transfer;
 import org.bnpparibas.rdb.model.operations.Withdrawal;
 import org.bnpparibas.rdb.service.implementation.TransactionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class TransactionController {
 
     @Autowired
     private TransactionServiceImpl transactionService;
 
-    @PostMapping("/deposit")
-    public ResponseEntity<Object> postDeposit(@RequestBody Deposit deposit, Long accountNumber) {
-        return transactionService.deposit(deposit, accountNumber);
+
+    @GetMapping("/operations")
+    public String showOperations(@ModelAttribute("account") Account account, Model model, HttpSession session) {
+
+        Client client = (Client) session.getAttribute("client");
+
+        if (client != null) {
+            model.addAttribute("client", client);
+            model.addAttribute("account", account);
+            return "operations";
+        } else {
+            return "redirect:/login";
+        }
     }
 
-    @PostMapping("/withdrawal")
-    public ResponseEntity<Object> postWithdrawal(@RequestBody Withdrawal withdrawal, Long accountNumber) {
-        return transactionService.withdraw(withdrawal, accountNumber);
+    @PostMapping("/operations/deposit")
+    public String postDeposit(@ModelAttribute("account") Account account, @RequestBody Deposit deposit, Long accountNumber) {
+        transactionService.deposit(deposit, accountNumber);
+        return "redirect:/operations";
     }
 
-    @PostMapping("/transfer")
-    public ResponseEntity<Object> postTransfer(@RequestBody Transfer transfer, Long accountNumber) {
-        return transactionService.transfer(transfer, accountNumber);
+    @PostMapping("/operations/withdrawal")
+    public String postWithdrawal(@ModelAttribute("account") Account account, @RequestBody Withdrawal withdrawal, Long accountNumber) {
+        transactionService.withdraw(withdrawal, accountNumber);
+        return "redirect:/operations";
+    }
+
+    @PostMapping("/operations/transfer")
+    public String postTransfer(@ModelAttribute("account") Account account, @RequestBody Transfer transfer, Long accountNumber) {
+        transactionService.transfer(transfer, accountNumber);
+        return "redirect:/operations";
     }
 
     @GetMapping("/transactions")
-    public List<Transaction> getFindTransactionsByAccountNumber(@RequestParam Long accountNumber) {
-        return transactionService.findTransanctionsByAccountNumber(accountNumber);
+    public String showTransactionsByAccountNumber(@ModelAttribute("account") Account account, @RequestParam Long accountNumber, Model
+            model, HttpSession session) {
+
+        Client client = (Client) session.getAttribute("client");
+
+        if (client != null) {
+            model.addAttribute("client", client);
+            model.addAttribute("account", account);
+            transactionService.findTransanctionsByAccountNumber(accountNumber);
+            return "transactions";
+
+        } else {
+            return "redirect:/login";
+        }
     }
 }
